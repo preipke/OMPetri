24fdce59db61cb63731bd3312ff126470ade35b5
preipke <philo.reipke@uni-bielefeld.de>
2016-12-15 08:03:50 +0100

    removed sample code, now using the related petri net data model.

--- GraphEditor/src/main/java/edu/unibi/agbi/gnius/core/dao/PetriNetDao.java
+++ GraphEditor/src/main/java/edu/unibi/agbi/gnius/core/dao/PetriNetDao.java
@@ -5,18 +5,7 @@
  */
 package edu.unibi.agbi.gnius.core.dao;
 
-import edu.unibi.agbi.gnius.core.model.entity.data.IDataNode;
-import edu.unibi.agbi.gnius.core.service.exception.NodeCreationException;
-
 import edu.unibi.agbi.petrinet.model.PetriNet;
-import edu.unibi.agbi.petrinet.entity.impl.Arc;
-import edu.unibi.agbi.petrinet.entity.PN_Element;
-import edu.unibi.agbi.petrinet.entity.impl.Place;
-import edu.unibi.agbi.petrinet.entity.impl.Transition;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
@@ -27,25 +16,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PetriNetDao extends PetriNet
 {
-    private static final Map<String,PN_Element> pnNodesInternalIds = new HashMap();
-    private static final Map<String,ArrayList<PN_Element>> pnNodesUserIds = new HashMap();
-    
-    public void add(IDataNode node) throws NodeCreationException {
-        
-        PN_Element.Type type = node.getElementType();
-        
-        switch(type) {
-            case PLACE:
-                add((Place) node);
-                break;
-            case TRANSITION:
-                add((Transition) node);
-                break;
-            case ARC:
-                add((Arc) node);
-                break;
-            default:
-                throw new NodeCreationException("No suitable node type selected!");
-        }
-    }
 }
